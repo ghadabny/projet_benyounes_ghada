993bca8a4198f479aee88907623247e6d52f22ad
Ghada Ben Younes <ghadabenyounes7@gmail.com>
2024-01-03 19:17:09 +0100

    m

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -5,8 +5,14 @@ export const environment = {
   // backendSearchCatalogue: 'http://localhost:443/api/catalogue/getSearchCatalogue',
   // backendInscriptionUrl:'http://localhost:443/api/utilisateur/inscrire',
 
-  backendLoginClient: 'https://tp6.onrender.com/api/utilisateur/login',
-  backendCatalogue: 'https://tp6.onrender.com/api/catalogue/get',
-  backendSearchCatalogue: 'https://tp6.onrender.com/api/catalogue/getSearchCatalogue',
-  backendInscriptionUrl:'https://tp6.onrender.com/api/utilisateur/inscrire',
+  // backendLoginClient: 'https://tp6.onrender.com/api/utilisateur/login',
+  // backendCatalogue: 'https://tp6.onrender.com/api/catalogue/get',
+  // backendSearchCatalogue: 'https://tp6.onrender.com/api/catalogue/getSearchCatalogue',
+  // backendInscriptionUrl:'https://tp6.onrender.com/api/utilisateur/inscrire',
+
+  backendLoginClient: 'https://tp7-6wpr.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/get',
+  backendSearchCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/getSearchCatalogue',
+  backendInscriptionUrl:'https://tp7-6wpr.onrender.com/api/utilisateur/inscrire',
 };
+
