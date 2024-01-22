10ed466532ab1dfb5cd3c19ff117302d2c5b0842
Ghada Ben Younes <ghadabenyounes7@gmail.com>
2024-01-22 09:44:18 +0100

    m

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,18 +1,16 @@
 export const environment = {
   production: false,
-  backendLoginClient: 'http://localhost:443/api/utilisateur/login',
-  backendCatalogue: 'http://localhost:443/api/catalogue/get',
-  backendSearchCatalogue: 'http://localhost:443/api/catalogue/getSearchCatalogue',
-  backendInscriptionUrl:'http://localhost:443/api/utilisateur/inscrire',
+  
+  // backendLoginClient: 'http://localhost:443/api/utilisateur/login',
+  // backendCatalogue: 'http://localhost:443/api/catalogue/get',
+  // backendSearchCatalogue: 'http://localhost:443/api/catalogue/getSearchCatalogue',
+  // backendInscriptionUrl:'http://localhost:443/api/utilisateur/inscrire',
 
-  // backendLoginClient: 'https://tp6.onrender.com/api/utilisateur/login',
-  // backendCatalogue: 'https://tp6.onrender.com/api/catalogue/get',
-  // backendSearchCatalogue: 'https://tp6.onrender.com/api/catalogue/getSearchCatalogue',
-  // backendInscriptionUrl:'https://tp6.onrender.com/api/utilisateur/inscrire',
+  backendLoginClient: 'https://tp7-6wpr.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/get',
+  backendSearchCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/getSearchCatalogue',
+  backendInscriptionUrl:'https://tp7-6wpr.onrender.com/api/utilisateur/inscrire',
+  
 
-  // backendLoginClient: 'https://tp7-6wpr.onrender.com/api/utilisateur/login',
-  // backendCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/get',
-  // backendSearchCatalogue: 'https://tp7-6wpr.onrender.com/api/catalogue/getSearchCatalogue',
-  // backendInscriptionUrl:'https://tp7-6wpr.onrender.com/api/utilisateur/inscrire',
 };
 
